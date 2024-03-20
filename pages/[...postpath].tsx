f5430b92aadf151e31cb3318fed8af7f1b108c28
zinghay <zinghay.com@gmail.com>
2024-03-20 17:11:55 +0700

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -86,9 +86,9 @@ const Post: React.FC<PostProps> = (props) => {
   {/* <meta property="og:title" content={post.title} />
     
    */}
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" /> 
-  <meta name="og:url" content={post.featuredImage.node.sourceUrl} />
+ <meta charset="UTF-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" /> 
+<meta name="og:url" content={post.featuredImage.node.sourceUrl} />
   <meta property="og:description" content="ㅤ" />
   <meta property="og:type" content="article" />
   <meta property="og:locale" content="en_US" />
