87e30bd7248e0991031778e3785a37ed3d0fc81b
zinghay <zinghay.com@gmail.com>
2024-03-30 13:38:06 +0700

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -118,11 +118,11 @@ const Post: React.FC<PostProps> = ({ post }) => {
       </div>
     
 
-                <div className="related-posts">
+            <div className="related-posts">
     <h2>Related Posts</h2>
     <div className="row">
         {post.categories?.nodes.map((category: any) => (
-            category.posts?.nodes.map((relatedPost: any) => (
+            category.posts?.nodes.slice(0, 6).map((relatedPost: any) => ( // Chỉ lấy 6 bài viết đầu tiên
                 <div className="col-md-4" key={relatedPost.id}>
                     <div className="item card">
                         <a href={relatedPost.link} className="card-link">
@@ -136,9 +136,10 @@ const Post: React.FC<PostProps> = ({ post }) => {
     </div>
 </div>
 
+
             <footer className={styles.footer}>
-                <a href="https://animalaz.netlify.app/" target="_blank" rel="noopener noreferrer" className={styles.title}>
-                    Powered by Animalaz
+                <a href="/" target="_blank" rel="noopener noreferrer" className={styles.title}>
+                    Powered by Park Ji Sung
                 </a>
             </footer>
 
