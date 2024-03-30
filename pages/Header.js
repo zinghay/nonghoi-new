711eba684cc85e00e026c26680970de1762f1e28
zinghay <zinghay.com@gmail.com>
2024-03-30 22:39:06 +0700

    Create Header.js

---
+++ pages/Header.js
@@ -0,0 +1,89 @@
+export default function Header() { return (
+  <header class="header_header__zJOD0 header_sticky__2ak2z">
+    <a class="skip-nav-link_skipLink__M2lut" href="#geist-skip-nav" tabindex="0">Skip to content</a>
+    <nav class="navbar_nav__dCdXi">
+      <div class="navbar_mobileTop__yLNXQ">
+        <div class="stack_stack__iZkUS stack" data-version="v1" style="--stack-flex:initial;--stack-direction:row;--stack-align:center;--stack-justify:flex-start;--stack-padding:0px;--stack-gap:4px">
+          <a rel="noopener noreferrer" target="_blank" data-testid="navbar/vercel-logo" href="https://vercel.com/home?utm_source=next-site&amp;utm_medium=banner&amp;utm_campaign=home" aria-label="Go to Vercel homepage" title="Go to Vercel homepage">
+            <svg aria-label="Vercel logomark" height="22" role="img" style="width:auto;overflow:visible" viewBox="0 0 74 64">
+              <path d="M37.5896 0.25L74.5396 64.25H0.639648L37.5896 0.25Z" fill="var(--geist-foreground)"></path>
+            </svg>
+          </a>
+          <svg height="32" role="separator" viewBox="0 0 32 32" width="32">
+            <path d="M22 5L9 28" stroke="var(--accents-2)" stroke-linecap="round" stroke-linejoin="round"></path>
+          </svg>
+          <a class="navbar_mobileLogo__Q5KxZ" title="Go to the homepage" href="/">
+            link anh
+          </a>
+        </div>
+        <div class="navbar_mobileTopRight__p4VGF">
+          <button class="navbar_search__dZT2b" data-variant="large" type="button">Search documentation... <kbd class="navbar_kbd__5R6Wu">⌘K</kbd>
+          </button>
+          <button class="navbar_search__dZT2b" data-variant="medium" type="button">Search... <kbd>⌘K</kbd>
+          </button>
+          <button aria-label="Search documentation" class="navbar_search__dZT2b" data-variant="small" type="button">
+            <svg class="with-icon_icon__MHUeb" data-testid="geist-icon" fill="none" height="24" shape-rendering="geometricPrecision" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" viewBox="0 0 24 24" width="24" style="color:currentColor">
+              <path d="M11 17.25a6.25 6.25 0 110-12.5 6.25 6.25 0 010 12.5z"></path>
+              <path d="M16 16l4.5 4.5"></path>
+            </svg>
+          </button>
+          <button aria-label="open menu" class="navbar_menuButton__BJwt4" type="button">
+            <div class="menu-toggle_wrap__qevaX "></div>
+          </button>
+        </div>
+      </div>
+      <div class="navbar_links__jysVN">
+        <div class="stack_stack__iZkUS stack" data-version="v1" style="--stack-flex:initial;--stack-direction:row;--stack-align:center;--stack-justify:flex-start;--stack-padding:0px;--stack-gap:4px">
+          <a rel="noopener noreferrer" target="_blank" data-testid="navbar/vercel-logo" href="https://vercel.com/home?utm_source=next-site&amp;utm_medium=banner&amp;utm_campaign=home" aria-label="Go to Vercel homepage" title="Go to Vercel homepage">
+            <svg aria-label="Vercel logomark" height="22" role="img" style="width:auto;overflow:visible" viewBox="0 0 74 64">
+              <path d="M37.5896 0.25L74.5396 64.25H0.639648L37.5896 0.25Z" fill="var(--geist-foreground)"></path>
+            </svg>
+          </a>
+          <svg height="32" role="separator" viewBox="0 0 32 32" width="32">
+            <path d="M22 5L9 28" stroke="var(--accents-2)" stroke-linecap="round" stroke-linejoin="round"></path>
+          </svg>
+          <a aria-label="Go to the homepage" class="navbar_logo__wXwXy" title="Go to the homepage" data-state="closed" style="-webkit-touch-callout:none" href="/">
+           trang chu
+          </a>
+        </div>
+        <a class="" href="/showcase">Showcase</a>
+        <a class="" title="Documentation" href="/docs">Docs</a>
+        <a class="" href="/blog">Blog</a>
+        <a rel="canonical noreferrer" target="_blank" href="https://vercel.com/analytics?utm_source=next-site&amp;utm_medium=navbar&amp;utm_campaign=next_site_nav_analytics">Analytics <svg aria-hidden="true" class="navbar_externalArrow___VWBd" height="7" viewBox="0 0 6 6" width="7">
+            <path d="M1.25215 5.54731L0.622742 4.9179L3.78169 1.75597H1.3834L1.38936 0.890915H5.27615V4.78069H4.40513L4.41109 2.38538L1.25215 5.54731Z" fill="var(--accents-3)"></path>
+          </svg>
+        </a>
+        <a rel="noopener noreferrer" target="_blank" href="https://vercel.com/templates/next.js?utm_source=next-site&amp;utm_medium=navbar&amp;utm_campaign=next_site_nav_templates">Templates <svg aria-hidden="true" class="navbar_externalArrow___VWBd" height="7" viewBox="0 0 6 6" width="7">
+            <path d="M1.25215 5.54731L0.622742 4.9179L3.78169 1.75597H1.3834L1.38936 0.890915H5.27615V4.78069H4.40513L4.41109 2.38538L1.25215 5.54731Z" fill="var(--accents-3)"></path>
+          </svg>
+        </a>
+        <a rel="noopener noreferrer" target="_blank" href="https://vercel.com/contact/sales/nextjs?utm_source=next-site&amp;utm_medium=navbar&amp;utm_campaign=next_site_nav_enterprise">Enterprise <svg aria-hidden="true" class="navbar_externalArrow___VWBd" height="7" viewBox="0 0 6 6" width="7">
+            <path d="M1.25215 5.54731L0.622742 4.9179L3.78169 1.75597H1.3834L1.38936 0.890915H5.27615V4.78069H4.40513L4.41109 2.38538L1.25215 5.54731Z" fill="var(--accents-3)"></path>
+          </svg>
+        </a>
+      </div>
+      <div class="navbar_headerButtons__m6ZFa">
+        <button class="navbar_search__dZT2b" data-variant="large" type="button">Search documentation... <kbd class="navbar_kbd__5R6Wu">⌘K</kbd>
+        </button>
+        <button class="navbar_search__dZT2b" data-variant="medium" type="button">Search... <kbd>⌘K</kbd>
+        </button>
+        <button aria-label="Search documentation" class="navbar_search__dZT2b" data-variant="small" type="button">
+          <svg class="with-icon_icon__MHUeb" data-testid="geist-icon" fill="none" height="24" shape-rendering="geometricPrecision" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" viewBox="0 0 24 24" width="24" style="color:currentColor">
+            <path d="M11 17.25a6.25 6.25 0 110-12.5 6.25 6.25 0 010 12.5z"></path>
+            <path d="M16 16l4.5 4.5"></path>
+          </svg>
+        </button>
+        <a role="link" tabindex="0" href="https://vercel.com/new/clone?utm_source=next-site&amp;utm_medium=banner&amp;b=main&amp;s=https%3A%2F%2Fgithub.com%2Fvercel%2Fvercel%2Ftree%2Fmain%2Fexamples%2Fnextjs&amp;showOptionalTeamCreation=false&amp;template=nextjs&amp;teamCreateStatus=hidden&amp;utm_campaign=home" type="submit" class="button_base__BjwbK reset_reset__KRyvc button_button__81573 reset_reset__KRyvc navbar_deploy__IKZN3 button_secondary__kMMNc button_small__iQMBm button_invert__YNhnn" data-geist-button="" data-prefix="true" data-suffix="false" data-version="v1" style="--geist-icon-size:16px">
+          <span class="button_prefix__2XlwH">
+            <svg aria-label="Vercel logomark" height="13" role="img" style="width:auto;overflow:visible" viewBox="0 0 74 64">
+              <path d="M37.5896 0.25L74.5396 64.25H0.639648L37.5896 0.25Z" fill="var(--geist-foreground)"></path>
+            </svg>
+          </span>
+          <span class="button_content__1aE1_">Deploy</span>
+        </a>
+        <a role="link" tabindex="0" href="/learn?utm_source=next-site&amp;utm_medium=navbar" type="submit" class="button_base__BjwbK reset_reset__KRyvc button_button__81573 reset_reset__KRyvc geist-new-themed geist-new-default geist-new-default-fill button_small__iQMBm button_invert__YNhnn" data-geist-button="" data-prefix="false" data-suffix="false" data-version="v1" style="--geist-icon-size:16px">
+          <span class="button_content__1aE1_">Learn</span>
+        </a>
+      </div>
+    </nav>
+</header> ) }
