51ebdeec885024c7e4f1f547c09b62baa0178c76
zinghay <zinghay.com@gmail.com>
2024-03-30 17:04:31 +0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -87,13 +87,50 @@ const Home: NextPage<HomeProps> = ({ posts }) => {
           ))}
         </div>
 
-        <div className={styles.pagination}>
-          {Array.from({ length: totalPages }, (_, index) => (
-            <Link href={`/?page=${index + 1}`} key={index}>
-              <a className={parseInt(page as string) === index + 1 ? styles.activePage : undefined}>{index + 1}</a>
-            </Link>
-          ))}
-        </div>
+<div class="pagination_rounded">
+                        <ul>
+                            <li>
+                                <a href="#" class="prev"> <i class="fa fa-angle-left" aria-hidden="true"></i> Prev </a>
+                            </li>
+                            <li><a href="#">1</a>
+                            </li>
+							<li class="hidden-xs"><a href="#">2</a>
+                            </li>
+                            <li class="hidden-xs"><a href="#">3</a>
+                            </li>
+                            <li class="hidden-xs"><a href="#">4</a>
+                            </li>
+                            <li class="hidden-xs"><a href="#">5</a>
+                            </li>
+							<li class="visible-xs"><a href="#">...</a>
+                            </li>
+							<li><a href="#">6</a>
+                            </li>
+                            <li><a href="#" class="next"> Next <i class="fa fa-angle-right" aria-hidden="true"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+
+
+        
+<div className={styles.pagination_rounded}>
+    <ul>
+        <li>
+            <a href="#" className="prev"> <i className="fa fa-angle-left" aria-hidden="true"></i> Prev </a>
+        </li>
+        {Array.from({ length: totalPages }, (_, index) => (
+            <li key={index}>
+                <Link href={`/?page=${index + 1}`}>
+                    <a className={parseInt(page as string) === index + 1 ? styles.activePage : undefined}>{index + 1}</a>
+                </Link>
+            </li>
+        ))}
+        <li>
+            <a href="#" className="next"> Next <i className="fa fa-angle-right" aria-hidden="true"></i></a>
+        </li>
+    </ul>
+</div>
+
       </main>
        <footer className={styles.footer}>
                 <a href="/" target="_blank" rel="noopener noreferrer" >
