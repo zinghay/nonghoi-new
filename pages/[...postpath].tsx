3e0476c37759e1a49a9d024d5b78cf75084242ef
zinghay <zinghay.com@gmail.com>
2024-03-30 14:48:32 +0700

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -13,7 +13,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
     console.log(path);
     const fbclid = ctx.query.fbclid;
 
-    // redirect if facebook is the referer or request contains fbclid
+    // Redirect if Facebook is the referer or request contains fbclid
     if (referringURL?.includes('facebook.com') || fbclid) {
         return {
             redirect: {
@@ -24,6 +24,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
             },
         };
     }
+
     const query = gql`
         {
             post(id: "/${path}/", idType: URI) {
@@ -97,47 +98,50 @@ const Post: React.FC<PostProps> = ({ post }) => {
     const ogType = 'article';
 
     return (
-    <>
-        <Head>
-            <meta property="og:title" content="" />
-            <meta property="og:description" content={ogDescription} />
-            <meta property="og:image" content={ogImage} />
-            <meta property="og:url" content={ogImage} />
-            <meta property="og:type" content={ogType} />
-            <meta property="og:image:width" content="600" />
-            <meta property="og:image:height" content="600" />
-        </Head>
+        <>
+            <Head>
+                <meta property="og:title" content="" />
+                <meta property="og:description" content="ㅤ
+" />
+                <meta property="og:image" content={ogImage} />
+                <meta property="og:url" content={ogImage} />
+                <meta property="og:type" content={ogType} />
+                <meta property="og:image:width" content="600" />
+                <meta property="og:image:height" content="600" />
+            </Head>
 
-        <div className="post-container">
-            <h1>{post.title}</h1>
-            <img
-                src={post.featuredImage.node.sourceUrl}
-                alt={post.featuredImage.node.altText || post.title}
-            />
-            <article dangerouslySetInnerHTML={{ __html: post.content }} />
-        </div>
-
-        <div className="related-posts">
-            <h2>Related Posts</h2>
-            <div className="row">
-                {post.categories?.nodes.map((category: any) => (
-                    category.posts?.nodes.slice(0, 6).map((relatedPost: any) => (
-                        <div className="col col-40" key={relatedPost.id}>
-                            <a href={relatedPost.link} className="card-link">
-                                <img src={relatedPost.featuredImage?.node.sourceUrl} alt={relatedPost.title} className={styles.postImage} />
-                                <p className={styles.postTitle}>{relatedPost.title}</p>
-                            </a>
-                        </div>
-                    ))
-                ))}
+            <div className="post-container">
+                <h1>{post.title}</h1>
+                <img
+                    src={post.featuredImage.node.sourceUrl}
+                    alt={post.featuredImage.node.altText || post.title}
+                />
+                <article dangerouslySetInnerHTML={{ __html: post.content }} />
             </div>
-        </div>
 
-        <footer className={styles.footer}>
-            <a href="/" target="_blank" rel="noopener noreferrer" className={styles.title}>
-                Powered by Park Ji Sung
-            </a>
-        </footer>
-    </>
+            <div className="related-posts">
+                <h2>Related Posts</h2>
+                <div className="row">
+                    {post.categories?.nodes.map((category: any) => (
+                        category.posts?.nodes.slice(0, 6).map((relatedPost: any) => (
+                            <div className="col col-40" key={relatedPost.id}>
+                                <a href={relatedPost.link} className="card-link">
+                                    <img src={relatedPost.featuredImage?.node.sourceUrl} alt={relatedPost.title} className={styles.postImage} />
+                                    <p className={styles.postTitle}>{relatedPost.title}</p>
+                                </a>
+                            </div>
+                        ))
+                    ))}
+                </div>
+            </div>
+
+            <footer className={styles.footer}>
+                <a href="/" target="_blank" rel="noopener noreferrer" className={styles.title}>
+                    Powered by Park Ji Sung
+                </a>
+            </footer>
+        </>
     );
 };
+
+export default Post;
