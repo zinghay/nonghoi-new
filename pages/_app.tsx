aed51b10f20aee7d0536cab81dbb67c4f98e82ef
zinghay <zinghay.com@gmail.com>
2024-03-30 22:42:22 +0700

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Header from '../pages/Header'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
