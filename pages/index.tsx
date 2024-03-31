0092118eccaeac36e59cad0cb6454ce5d6024f2f
zinghay <zinghay.com@gmail.com>
2024-03-31 12:29:50 +0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,6 @@
-
 import type { GetServerSideProps, NextPage } from 'next';
 import Head from 'next/head';
 import Link from 'next/link';
-import { useRouter } from 'next/router';
 import { GraphQLClient, gql } from 'graphql-request';
 import styles from '../styles/Home.module.css';
 
@@ -28,55 +26,33 @@ interface Post {
 
 interface HomeProps {
   posts: Post[];
+  currentPage: number;
+  totalPages: number;
 }
 
-const Home: NextPage<HomeProps> = ({ posts }) => {
-  const router = useRouter();
-  const { page } = router.query;
-
-  // Số bài viết hiển thị trên mỗi trang
-  const postsPerPage = 12;
-
-  // Tính toán số trang
-  const totalPages = Math.ceil(posts.length / postsPerPage);
-
-  // Lấy chỉ mục bắt đầu và kết thúc của bài viết trang hiện tại
-  const startIndex = (parseInt(page as string) - 1) * postsPerPage;
-  const endIndex = startIndex + postsPerPage;
-
-  // Lấy danh sách bài viết trang hiện tại
-  const currentPosts = posts.slice(startIndex, endIndex);
-  
-  const currentPage = 1; // For example, currentPage is assigned the value 1
-
-
+const Home: NextPage<HomeProps> = ({ posts, currentPage, totalPages }) => {
   return (
-
     <div className={styles.container}>
-    <Head>
-      <title>Blog</title>
-      <link rel="icon" href="https://actualidadradio.com/favicon.ico/ms-icon-310x310.png" />
-    </Head>
+      <Head>
+        <title>Blog</title>
+        <link rel="icon" href="https://actualidadradio.com/favicon.ico/ms-icon-310x310.png" />
+      </Head>
 
-    <header className={styles.header}>
-      <a href="/"><h2>HOME</h2></a>
-    </header>
-    <div className={styles.topnav}>
-      <a href="#">Animal</a> 
-      <form className={`${styles["search-form"]} my-2 my-lg-0`} role="search" method="get" action="">
-        <div className={styles["input-group"]}>
-          <input type="text" name="s" className={styles["form-control"]} placeholder="Search" title="Search" />
-          <button type="submit" name="submit" className={`${styles.btn} ${styles["btn-outline-secondary"]}`}>Search</button>
-        </div>
-      </form>
-    </div>
-  </div>
+      <header className={styles.header}>
+        <a href="/"><h2>HOME</h2></a>
+      </header>
+      <div className={styles.topnav}>
+        <a href="#">Animal</a>
+        <form className={`${styles["search-form"]} my-2 my-lg-0`} role="search" method="get" action="">
+          <div className={styles["input-group"]}>
+            <input type="text" name="s" className={styles["form-control"]} placeholder="Search" title="Search" />
+            <button type="submit" name="submit" className={`${styles.btn} ${styles["btn-outline-secondary"]}`}>Search</button>
+          </div>
+        </form>
+      </div>
 
-
-
-
-      <main className={styles.main}> 
-  <div className={styles.postGrid}>
+      <main className={styles.main}>
+        <div className={styles.postGrid}>
           {posts.map((post) => (
             <div key={post.id} className={styles.postCard}>
               <Link href={post.link}>
@@ -99,42 +75,35 @@ const Home: NextPage<HomeProps> = ({ posts }) => {
           ))}
         </div>
 
-
-
-        
-<div className={styles.pagination_rounded}>
-    <ul>
-        <li>
-            <a href={`/?page=${currentPage > 1 ? currentPage - 1 : 1}`} className="prev">
+        <div className={styles.pagination_rounded}>
+          <ul>
+            <li>
+              <a href={`/?page=${currentPage > 1 ? currentPage - 1 : 1}`} className="prev">
                 <i className="fa fa-angle-left" aria-hidden="true"></i> Prev
-            </a>
-        </li>
-        {Array.from({ length: totalPages }, (_, index) => (
-            <li key={index}>
-                <Link href={`/?page=${index + 1}`}>
-                    <a className={parseInt(page as string) === index + 1 ? styles.activePage : undefined}>{index + 1}</a>
-                </Link>
+              </a>
             </li>
-        ))}
-        <li>
-            <a href={`/?page=${currentPage < totalPages ? currentPage + 1 : totalPages}`} className="next">
+            {Array.from({ length: totalPages }, (_, index) => (
+              <li key={index}>
+                <Link href={`/?page=${index + 1}`}>
+                  <a className={currentPage === index + 1 ? styles.activePage : undefined}>{index + 1}</a>
+                </Link>
+              </li>
+            ))}
+            <li>
+              <a href={`/?page=${currentPage < totalPages ? currentPage + 1 : totalPages}`} className="next">
                 Next <i className="fa fa-angle-right" aria-hidden="true"></i>
-            </a>
-        </li>
-    </ul>
-</div>
-
-
-
-
+              </a>
+            </li>
+          </ul>
+        </div>
       </main>
-       <footer className={styles.footer}>
-                <a href="/" rel="noopener noreferrer" >
-                    Powered by Park Ji Sung
-                </a>
-            </footer>
-    </div>
 
+      <footer className={styles.footer}>
+        <a href="/" rel="noopener noreferrer" >
+          Powered by Park Ji Sung
+        </a>
+      </footer>
+    </div>
   );
 };
 
@@ -174,9 +143,15 @@ export const getServerSideProps: GetServerSideProps = async ({ req }) => {
     link: `${baseUrl}/${post.uri}`,
   }));
 
+  const postsPerPage = 12;
+  const totalPages = Math.ceil(posts.length / postsPerPage);
+  const currentPage = 1;
+
   return {
     props: {
-      posts,
+      posts: posts.slice(0, postsPerPage),
+      currentPage,
+      totalPages,
     },
   };
 };
